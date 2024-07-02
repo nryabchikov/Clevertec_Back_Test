2c03688364b25d7a71d96c408a33d34ab8f06946
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-03 00:13:07 +0300

    refactor: return type(method readProducts from csv file) from map to list(not needed ids of products)

--- src/main/java/ru/clevertec/check/util/CsvReader.java
+++ src/main/java/ru/clevertec/check/util/CsvReader.java
@@ -3,20 +3,21 @@ package main.java.ru.clevertec.check.util;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CsvReader {
     private static final String SEPARATOR = ";";
-    public static Map<Integer, String[]> readProducts(String filePath) throws IOException {
-        Map<Integer, String[]> products = new HashMap<>();
+    public static List<String[]> readProducts(String filePath) throws IOException {
+        List<String[]> products = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] product = line.split(SEPARATOR);
-                int id = Integer.parseInt(product[0]);
-                products.put(id, product);
+                products.add(product);
             }
         }
         return products;
@@ -26,6 +27,7 @@ public class CsvReader {
         Map<Integer, Integer> discountCards = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
+            br.readLine();
             while ((line = br.readLine()) != null) {
                 String[] card = line.split(SEPARATOR);
                 int cardNumber = Integer.parseInt(card[1]);
