f139eec2952687d364bde9927082d154f3b65c30
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-02 23:29:18 +0300

    feat: getters, setters, constructors

--- src/main/java/ru/clevertec/check/model/DiscountCard.java
+++ src/main/java/ru/clevertec/check/model/DiscountCard.java
@@ -1,10 +1,37 @@
 package main.java.ru.clevertec.check.model;
 
-import lombok.Data;
-
-@Data
 public class DiscountCard {
     private int id;
     private int number;
     private int discount;
+
+    public DiscountCard(int id, int number, int discount) {
+        this.id = id;
+        this.number = number;
+        this.discount = discount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
 }
--- src/main/java/ru/clevertec/check/model/Product.java
+++ src/main/java/ru/clevertec/check/model/Product.java
@@ -1,11 +1,47 @@
 package main.java.ru.clevertec.check.model;
 
-import lombok.Data;
-
-@Data
 public class Product {
     private int id;
     private String description;
     private double price;
     private boolean isWholesale;
+
+    public Product(int id, String description, double price, boolean isWholesale) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+        this.isWholesale = isWholesale;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isWholesale() {
+        return isWholesale;
+    }
+
+    public void setWholesale(boolean wholesale) {
+        isWholesale = wholesale;
+    }
 }
