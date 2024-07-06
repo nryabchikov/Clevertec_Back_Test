7be7480b99bb5c79e5f8b4831ffc0e72a6d09019
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-06 22:30:37 +0300

    feat: cut off the fractional part to 2 characters

---
+++ src/main/java/ru/clevertec/check/util/MathRounder.java
@@ -0,0 +1,7 @@
+package main.java.ru.clevertec.check.util;
+
+public class MathRounder {
+    public static double roundNumberToTwoDigits(double number) {
+        return Math.floor(number * 100) / 100.0;
+    }
+}
