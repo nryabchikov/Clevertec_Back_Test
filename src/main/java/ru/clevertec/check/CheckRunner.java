723cde53cea629cdd6474b22beaf3c8e2503c4ca
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-06 23:43:11 +0300

    feat: exception handler(handle my custom exceptions)

--- src/main/java/ru/clevertec/check/CheckRunner.java
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -1,5 +1,9 @@
 package main.java.ru.clevertec.check;
 
+import main.java.ru.clevertec.check.exception.DiscountCardNotFoundException;
+import main.java.ru.clevertec.check.exception.MissingBalanceException;
+import main.java.ru.clevertec.check.exception.MissingProductsException;
+import main.java.ru.clevertec.check.exception.ProductLackOfQuantityException;
 import main.java.ru.clevertec.check.model.Check;
 import main.java.ru.clevertec.check.service.CheckService;
 import main.java.ru.clevertec.check.util.CsvWriter;
@@ -10,21 +14,30 @@ import java.io.IOException;
 
 public class CheckRunner {
     public static void main(String[] args) throws IOException {
-        final String OUTPUT_FILEPATH = "result.csv";
+        final String OUTPUT_FILE_PATH = "result.csv";
         final String PRODUCTS_FILEPATH = "src/main/resources/products.csv";
         final String DISCOUNT_CARDS_FILEPATH = "src/main/resources/discountCards.csv";
-        InputHandler inputHandler = new InputHandler(args, PRODUCTS_FILEPATH, DISCOUNT_CARDS_FILEPATH);
-        CheckService checkService = new CheckService(PRODUCTS_FILEPATH);
-        Check check = checkService.generateCheck(
-                inputHandler.getMapOfIdAndAmountOfProducts(),
-                inputHandler.getNumberOfDiscountCard(),
-                inputHandler.getDiscountPercentage(),
-                inputHandler.getBalanceDebitCard()
-        );
-        if (inputHandler.getBalanceDebitCard() < check.getTotalPriceWithDiscount()) {
-            CsvWriter.writeErrorToCsv(Status.NOT_ENOUGH_MONEY, OUTPUT_FILEPATH);
-        } else {
-            CsvWriter.writeCheckToCsv(check, OUTPUT_FILEPATH);
+        try {
+            InputHandler inputHandler = new InputHandler(args, PRODUCTS_FILEPATH, DISCOUNT_CARDS_FILEPATH);
+            CheckService checkService = new CheckService(PRODUCTS_FILEPATH);
+            Check check = checkService.generateCheck(
+                    inputHandler.getMapOfIdAndAmountOfProducts(),
+                    inputHandler.getNumberOfDiscountCard(),
+                    inputHandler.getDiscountPercentage(),
+                    inputHandler.getBalanceDebitCard()
+            );
+            if (inputHandler.getBalanceDebitCard() < check.getTotalPriceWithDiscount()) {
+                System.err.println(Status.NOT_ENOUGH_MONEY.getMessage());
+                CsvWriter.writeErrorToCsv(Status.NOT_ENOUGH_MONEY, OUTPUT_FILE_PATH);
+            } else {
+                CsvWriter.writeCheckToCsv(check, OUTPUT_FILE_PATH);
+            }
+        } catch (DiscountCardNotFoundException | MissingBalanceException | MissingProductsException |
+                 ProductLackOfQuantityException e) {
+            System.err.println(e.getMessage());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            CsvWriter.writeErrorToCsv(Status.INTERNAL_SERVER_ERROR, OUTPUT_FILE_PATH);
         }
     }
 }
