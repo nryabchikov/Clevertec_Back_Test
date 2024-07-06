8d494527847bf690fcfc1b3846a790c98b189098
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-06 22:28:34 +0300

    feat: exception(not enough quantity of product)

---
+++ src/main/java/ru/clevertec/check/exception/ProductLackOfQuantityException.java
@@ -0,0 +1,7 @@
+package main.java.ru.clevertec.check.exception;
+
+public class ProductLackOfQuantityException extends RuntimeException {
+    public ProductLackOfQuantityException(String message) {
+        super(message);
+    }
+}
