ecc92fbd0283c4019e19a54de4226f59c97e3e8b
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-06 22:44:14 +0300

    feat: main class(CheckRunner)

--- src/main/java/ru/clevertec/check/CheckRunner.java
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -1,7 +1,31 @@
 package main.java.ru.clevertec.check;
 
-public class CheckRunner {
-    public static void main(String[] args) {
+import main.java.ru.clevertec.check.model.Check;
+import main.java.ru.clevertec.check.service.CheckService;
+import main.java.ru.clevertec.check.util.CsvWriter;
+import main.java.ru.clevertec.check.util.InputHandler;
+import main.java.ru.clevertec.check.util.Status;
 
+import java.io.IOException;
+
+public class CheckRunner {
+    public static void main(String[] args) throws IOException {
+        final String OUTPUT_FILEPATH = "result.csv";
+        final String PRODUCTS_FILEPATH = "src/main/resources/products.csv";
+        final String DISCOUNT_CARDS_FILEPATH = "src/main/resources/discountCards.csv";
+        InputHandler inputHandler = new InputHandler(args, PRODUCTS_FILEPATH, DISCOUNT_CARDS_FILEPATH);
+        CheckService checkService = new CheckService(PRODUCTS_FILEPATH);
+        Check check = checkService.generateCheck(
+                inputHandler.getMapOfIdAndAmountOfProducts(),
+                inputHandler.getNumberOfDiscountCard(),
+                inputHandler.getDiscountPercentage(),
+                inputHandler.getBalanceDebitCard()
+        );
+        if (inputHandler.getBalanceDebitCard() < check.getTotalPriceWithDiscount()) {
+            CsvWriter.writeErrorToCsv(Status.NOT_ENOUGH_MONEY, OUTPUT_FILEPATH);
+        } else {
+            CsvWriter.writeCheckToCsv(check, OUTPUT_FILEPATH);
+        }
     }
 }
+
