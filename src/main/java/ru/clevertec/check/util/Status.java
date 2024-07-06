bec24b164bff5ae75bcaa8ee9400af2200ea01c6
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-06 22:37:24 +0300

    feat: different status for exception

---
+++ src/main/java/ru/clevertec/check/util/Status.java
@@ -0,0 +1,17 @@
+package main.java.ru.clevertec.check.util;
+
+public enum Status {
+    BAD_REQUEST("BAD REQUEST"),
+    NOT_ENOUGH_MONEY("NOT ENOUGH MONEY"),
+    INTERNAL_SERVER_ERROR("INTERNAL SERVER ERROR");
+
+    private final String message;
+
+    Status(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
