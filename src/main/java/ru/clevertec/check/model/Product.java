6a371583bc5ff6888c8c1c69111ab48af0c141df
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-06 22:34:40 +0300

    feat: quantity in stock

--- src/main/java/ru/clevertec/check/model/Product.java
+++ src/main/java/ru/clevertec/check/model/Product.java
@@ -2,46 +2,31 @@ package main.java.ru.clevertec.check.model;
 
 public class Product {
     private int id;
-    private String description;
-    private double price;
-    private boolean isWholesale;
+    private final String description;
+    private final double price;
+    private final int quantityInStock;
+    private final boolean isWholesale;
 
-    public Product(int id, String description, double price, boolean isWholesale) {
+
+    public Product(int id, String description, double price, int quantityInStock, boolean isWholesale) {
         this.id = id;
         this.description = description;
         this.price = price;
+        this.quantityInStock = quantityInStock;
         this.isWholesale = isWholesale;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public int getQuantityInStock() {
+        return quantityInStock;
     }
 
     public String getDescription() {
         return description;
     }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public double getPrice() {
         return price;
     }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public boolean isWholesale() {
         return isWholesale;
-    }
-
-    public void setWholesale(boolean wholesale) {
-        isWholesale = wholesale;
     }
 }
