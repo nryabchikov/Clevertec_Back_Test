157fd00d52a03e59b2c4bf9806da67b6ff746cd6
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-07 21:15:57 +0300

    feat: validate path and save to file

--- src/main/java/ru/clevertec/check/util/InputValidator.java
+++ src/main/java/ru/clevertec/check/util/InputValidator.java
@@ -2,12 +2,14 @@ package main.java.ru.clevertec.check.util;
 
 import main.java.ru.clevertec.check.exception.MissingBalanceException;
 import main.java.ru.clevertec.check.exception.MissingProductsException;
+import main.java.ru.clevertec.check.exception.PathToFileNotFoundException;
+import main.java.ru.clevertec.check.exception.SaveToFileNotFoundException;
 
 import java.io.IOException;
 
 public class InputValidator {
-     static void validateArgs(boolean isBalanceDebitCardSet, boolean isEmptyMap) throws IOException {
-        final String OUTPUT_FILE_PATH = "result.csv";
+     static void validateArgs(boolean isBalanceDebitCardSet, boolean isEmptyMap, boolean isProductFilePathSet,
+                              boolean isSaveToFilePathSet, String OUTPUT_FILE_PATH) throws IOException {
         if (!isBalanceDebitCardSet) {
             CsvWriter.writeErrorToCsv(Status.BAD_REQUEST, OUTPUT_FILE_PATH);
             throw new MissingBalanceException("Balance debit card not provided.");
@@ -16,6 +18,16 @@ public class InputValidator {
         if (isEmptyMap) {
             CsvWriter.writeErrorToCsv(Status.BAD_REQUEST, OUTPUT_FILE_PATH);
             throw new MissingProductsException("No products provided.");
+        }
+
+        if (!isProductFilePathSet) {
+            CsvWriter.writeErrorToCsv(Status.BAD_REQUEST, OUTPUT_FILE_PATH);
+            throw new PathToFileNotFoundException("Path to file with products not provided.");
+        }
+
+        if (!isSaveToFilePathSet) {
+            CsvWriter.writeErrorToCsv(Status.BAD_REQUEST, OUTPUT_FILE_PATH);
+            throw new SaveToFileNotFoundException("Path to file to save not provided.");
         }
     }
 }
