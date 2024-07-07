f155c63af776431fadd76cb29fdd89d91b5f50ac
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-07 11:58:54 +0300

    feat: input validator(validate input data)

---
+++ src/main/java/ru/clevertec/check/util/InputValidator.java
@@ -0,0 +1,21 @@
+package main.java.ru.clevertec.check.util;
+
+import main.java.ru.clevertec.check.exception.MissingBalanceException;
+import main.java.ru.clevertec.check.exception.MissingProductsException;
+
+import java.io.IOException;
+
+public class InputValidator {
+     static void validateArgs(boolean isBalanceDebitCardSet, boolean isEmptyMap) throws IOException {
+        final String OUTPUT_FILE_PATH = "result.csv";
+        if (!isBalanceDebitCardSet) {
+            CsvWriter.writeErrorToCsv(Status.BAD_REQUEST, OUTPUT_FILE_PATH);
+            throw new MissingBalanceException("Balance debit card not provided.");
+        }
+
+        if (isEmptyMap) {
+            CsvWriter.writeErrorToCsv(Status.BAD_REQUEST, OUTPUT_FILE_PATH);
+            throw new MissingProductsException("No products provided.");
+        }
+    }
+}
