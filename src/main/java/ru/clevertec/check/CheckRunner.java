e1164cbb27c632b19853c5371cb8055ea689a8d8
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-07 21:18:12 +0300

    refactor: new interface to work with path and save to file

--- src/main/java/ru/clevertec/check/CheckRunner.java
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -1,9 +1,6 @@
 package main.java.ru.clevertec.check;
 
-import main.java.ru.clevertec.check.exception.DiscountCardNotFoundException;
-import main.java.ru.clevertec.check.exception.MissingBalanceException;
-import main.java.ru.clevertec.check.exception.MissingProductsException;
-import main.java.ru.clevertec.check.exception.ProductLackOfQuantityException;
+import main.java.ru.clevertec.check.exception.*;
 import main.java.ru.clevertec.check.model.Check;
 import main.java.ru.clevertec.check.service.CheckService;
 import main.java.ru.clevertec.check.util.CsvWriter;
@@ -14,12 +11,12 @@ import java.io.IOException;
 
 public class CheckRunner {
     public static void main(String[] args) throws IOException {
-        final String OUTPUT_FILE_PATH = "result.csv";
-        final String PRODUCTS_FILEPATH = "src/main/resources/products.csv";
         final String DISCOUNT_CARDS_FILEPATH = "src/main/resources/discountCards.csv";
+        String defaultSaveFilePath = "result.csv";
         try {
-            InputHandler inputHandler = new InputHandler(args, PRODUCTS_FILEPATH, DISCOUNT_CARDS_FILEPATH);
-            CheckService checkService = new CheckService(PRODUCTS_FILEPATH);
+            InputHandler inputHandler = new InputHandler(args, DISCOUNT_CARDS_FILEPATH);
+            defaultSaveFilePath = inputHandler.getSaveFilePath();
+            CheckService checkService = new CheckService(inputHandler.getProductFilePath());
             Check check = checkService.generateCheck(
                     inputHandler.getMapOfIdAndAmountOfProducts(),
                     inputHandler.getNumberOfDiscountCard(),
@@ -28,17 +25,16 @@ public class CheckRunner {
             );
             if (inputHandler.getBalanceDebitCard() < check.getTotalPriceWithDiscount()) {
                 System.err.println(Status.NOT_ENOUGH_MONEY.getMessage());
-                CsvWriter.writeErrorToCsv(Status.NOT_ENOUGH_MONEY, OUTPUT_FILE_PATH);
+                CsvWriter.writeErrorToCsv(Status.NOT_ENOUGH_MONEY, inputHandler.getSaveFilePath());
             } else {
-                CsvWriter.writeCheckToCsv(check, OUTPUT_FILE_PATH);
+                CsvWriter.writeCheckToCsv(check, inputHandler.getSaveFilePath());
             }
         } catch (DiscountCardNotFoundException | MissingBalanceException | MissingProductsException |
-                 ProductLackOfQuantityException e) {
+                 ProductLackOfQuantityException | PathToFileNotFoundException | SaveToFileNotFoundException e) {
             System.err.println(e.getMessage());
         } catch (Exception e) {
             System.err.println(e.getMessage());
-            CsvWriter.writeErrorToCsv(Status.INTERNAL_SERVER_ERROR, OUTPUT_FILE_PATH);
+            CsvWriter.writeErrorToCsv(Status.INTERNAL_SERVER_ERROR, defaultSaveFilePath);
         }
     }
 }
-
