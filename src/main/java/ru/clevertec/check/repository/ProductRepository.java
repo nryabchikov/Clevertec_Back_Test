bc4b4caa00db06d1e0380cac27ec7e8d09cc650f
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-08 22:06:58 +0300

    refactor: product repository work with db

--- src/main/java/ru/clevertec/check/repository/ProductRepository.java
+++ src/main/java/ru/clevertec/check/repository/ProductRepository.java
@@ -1,41 +1,23 @@
 package ru.clevertec.check.repository;
 
-import ru.clevertec.check.exception.ProductNotFoundException;
+import ru.clevertec.check.mapper.ProductMapper;
 import ru.clevertec.check.model.Product;
-import ru.clevertec.check.util.CsvReader;
-import ru.clevertec.check.util.CsvWriter;
-import ru.clevertec.check.util.Status;
+import ru.clevertec.check.util.DatabaseConnection;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.sql.*;
 
 public class ProductRepository {
-    private final Map<Integer, Product> products = new HashMap<>();
-
-    public ProductRepository(String filePath) throws IOException {
-        loadProductsFromFile(filePath);
-    }
-
-    private void loadProductsFromFile(String filePath) throws IOException {
-        List<String[]> mapOfStringProduct = CsvReader.readProducts(filePath);
-        for (String[] stringProduct: mapOfStringProduct) {
-            int id = Integer.parseInt(stringProduct[0]);
-            String description = stringProduct[1];
-            double price = Double.parseDouble(stringProduct[2].replace(",", "."));
-            int quantityInStock = Integer.parseInt(stringProduct[3]);
-            boolean isWholesale = Objects.equals(stringProduct[4], "+");
-            products.put(id, new Product(id, description, price, quantityInStock, isWholesale));
+    public Product getProductById(int id) throws SQLException {
+        Product product = null;
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM product WHERE id=?")) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    product = ProductMapper.mapToProduct(resultSet);
+                }
+            }
         }
-    }
-
-    public Product findProductById(int id) throws IOException {
-        if (!(products.containsKey(id))) {
-            CsvWriter.writeErrorToCsv(Status.BAD_REQUEST, "result.csv");
-            throw new ProductNotFoundException("Product with id: " + id + " not found.");
-        }
-        return products.get(id);
+        return product;
     }
 }
