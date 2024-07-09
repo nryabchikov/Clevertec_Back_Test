bce36e367f5f374893467136ac435a8d2f196b0d
Nikita Ryabchikov <nryabchikov24@gmail.com>
2024-07-09 20:30:43 +0300

    refactor: main class for test

--- src/main/java/ru/clevertec/check/CheckRunner.java
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -2,7 +2,10 @@ package ru.clevertec.check;
 
 import ru.clevertec.check.exception.*;
 import ru.clevertec.check.model.Check;
+import ru.clevertec.check.repository.CheckRepository;
+import ru.clevertec.check.repository.ProductRepository;
 import ru.clevertec.check.service.CheckService;
+import ru.clevertec.check.service.ProductService;
 import ru.clevertec.check.util.CsvWriter;
 import ru.clevertec.check.util.InputHandler;
 import ru.clevertec.check.util.Status;
@@ -11,13 +14,13 @@ import java.io.IOException;
 
 public class CheckRunner {
     public static void main(String[] args) throws IOException {
-        args = new String[]{"3-1", "2-5", "5-1", "discountCard=1111", "balanceDebitCard=100", "saveToFile=result1.csv",
+        args = new String[]{"3-1", "2-5", "5-1", "discountCard=1111", "balanceDebitCard=100", "saveToFile=result.csv",
         "datasource.url=jdbc:postgresql://localhost:5432/clevertec_db", "datasource.username=nikitaryabchikov", "datasource.password=123"};
         String defaultSaveFilePath = "result.csv";
         try {
             InputHandler inputHandler = new InputHandler(args);
             defaultSaveFilePath = inputHandler.getSaveFilePath();
-            CheckService checkService = new CheckService();
+            CheckService checkService = new CheckService(new CheckRepository(new ProductService(new ProductRepository())));
             Check check = checkService.generateCheck(
                     inputHandler.getMapOfIdAndAmountOfProducts(),
                     inputHandler.getNumberOfDiscountCard(),
